66e9627cd62389e3e6736e8fb4c76c1451857bbe
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-03-27 00:20:13 -0700

    updated data js

--- src/data.js
+++ src/data.js
@@ -1,2 +1,13 @@
 import data from './data.json';
+
+// Part 1.1: Extract all categories using map
+const allCategories = data.map(item => item.category);
+
+// Part 1.2: Get unique categories using Set
+const uniqueCategories = [...new Set(allCategories)];
+
+// Export the data and processed results
 export default data;
+export {
+  uniqueCategories,
+};
